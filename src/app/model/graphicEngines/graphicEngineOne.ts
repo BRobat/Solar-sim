5f6b283e973610458dfa5b0e9085d156de122e84
BartTabor <bartlomiej.tabor@euvic.pl>
2022-05-17 20:32:28 +0200

    added camera position control

--- src/app/model/graphicEngines/graphicEngineOne.ts
+++ src/app/model/graphicEngines/graphicEngineOne.ts
@@ -17,13 +17,15 @@ export class GraphicEngineOne {
             ctx.fillStyle = 'rgb(' + (e.mass) / 30 + ',' + 0 + ',' + (100) + ')';
             ctx.lineWidth = 3
 
-            
+            let rx = camera.position.x - e.position.x;
+            let ry = camera.position.y - e.position.y;
+            let rz = camera.position.z - e.position.z;
 
 
-            const px = ((Math.tan(camera.angleOfView) * ((camera.position.x - e.position.x) / (camera.position.z - e.position.z))) * ctx.canvas.width / 4) + ctx.canvas.width / 4;
-            const py = ((Math.tan(camera.angleOfView) * ((camera.position.y - e.position.y) / (camera.position.z - e.position.z))) * ctx.canvas.height / 4) + ctx.canvas.height / 4;
+            const px = ((Math.tan(camera.angleOfView) * ((rx) / (rz))) * ctx.canvas.width / 4) + ctx.canvas.width / 4;
+            const py = ((Math.tan(camera.angleOfView) * ((ry) / (rz))) * ctx.canvas.height / 4) + ctx.canvas.height / 4;
             // taht needs to be changed
-            const pd = e.diameter * (50 / Math.sqrt(camera.position.z - e.position.z));
+            const pd = e.diameter * (50 / Math.sqrt(rz));
             ctx.arc(
                 px,
                 py,
