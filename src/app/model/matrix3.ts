ac4801b1eeedfe285b83aa58fbe86093b3c844dd
BartTabor <bartlomiej.tabor@euvic.pl>
2022-05-17 21:05:35 +0200

    wip

--- src/app/model/matrix3.ts
+++ src/app/model/matrix3.ts
@@ -1,6 +1,21 @@
 export class Matrix3 {
-    
+
+    aa: number;
+    ab: number;
+    ac: number;
+    ba: number;
+    bb: number;
+    bc: number;
+    ca: number;
+    cb: number;
+    cc: number;
+
     det(): number {
-        return 
+        return this.aa * this.bb * this.cc +
+            this.ab * this.bc * this.ca +
+            this.ac * this.ba * this.cb -
+            this.aa * this.bc * this.cb -
+            this.ab * this.ba * this.cc -
+            this.ac * this.bb * this.ca
     }
 }
